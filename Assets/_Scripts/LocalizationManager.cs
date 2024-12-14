190c347deab4dce4e5e4d62cf46e899ab7edbb99
ErikGames88 <erik.pan35@gmail.com>
2024-12-14 11:19:57 +0100

    LocalizationManager

---
+++ Assets/_Scripts/LocalizationManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocalizationManager : MonoBehaviour
+{
+    public static LocalizationManager Instance; // Singleton para acceso global
+
+    public enum Language { Spanish, English }
+    public Language currentLanguage = Language.Spanish;
+
+    private Dictionary<string, string> spanishTexts = new Dictionary<string, string>();
+    private Dictionary<string, string> englishTexts = new Dictionary<string, string>();
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); // Persistir entre escenas
+            InitializeDictionaries();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void InitializeDictionaries()
+    {
+        // Textos en español
+        spanishTexts.Add("Flashlight", "Linterna");
+        spanishTexts.Add("Skulls", "Cráneos");
+
+        // Textos en inglés
+        englishTexts.Add("Flashlight", "Flashlight");
+        englishTexts.Add("Skulls", "Skulls");
+    }
+
+    public string GetLocalizedText(string key)
+    {
+        switch (currentLanguage)
+        {
+            case Language.Spanish:
+                return spanishTexts.ContainsKey(key) ? spanishTexts[key] : key;
+            case Language.English:
+                return englishTexts.ContainsKey(key) ? englishTexts[key] : key;
+            default:
+                return key; // Retorna la clave si no encuentra el texto
+        }
+    }
+
+    public void SetLanguage(Language language)
+    {
+        currentLanguage = language;
+    }
+}
