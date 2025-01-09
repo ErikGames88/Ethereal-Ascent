b77d37e3b6df46c3c31d3d232831a4071989cd98
ErikGames88 <erik.pan35@gmail.com>
2025-01-09 21:07:07 +0100

    Pause Menu and Timer

--- Assets/_Scripts/PauseMenuManager.cs
+++ Assets/_Scripts/PauseMenuManager.cs
@@ -10,8 +10,20 @@ public class PauseMenuManager : MonoBehaviour
     [SerializeField]
     private PlayerController playerController; // Referencia al PlayerController
 
+    [SerializeField]
+    private MissionManager missionManager; // Referencia al MissionManager para comprobar si el MissionText se ha cerrado
+
+    [SerializeField]
+    private TimerManager timerManager; // Referencia al TimerManager para congelar el timer
+
     void Update()
     {
+        // Bloquear la apertura del Pause Menu hasta que el MissionText se haya cerrado
+        if (missionManager != null && !missionManager.isMissionTextClosed)
+        {
+            return; // No se puede abrir el Pause Menu hasta que el MissionText esté cerrado
+        }
+
         // Solo activar el menú de pausa si no está abierto el inventario
         if (Input.GetKeyDown(KeyCode.Escape) && !InventoryToggle.isInventoryOpen)
         {
@@ -35,6 +47,9 @@ public class PauseMenuManager : MonoBehaviour
             // Congelar el movimiento del jugador
             playerController.GetComponent<Rigidbody>().isKinematic = true;
 
+            // Congelar el Timer
+            timerManager.StopTimer();
+
             // Mostrar el cursor cuando el Pause Menu está activo
             Cursor.visible = true;
             Cursor.lockState = CursorLockMode.None; // Liberar el cursor
@@ -45,6 +60,9 @@ public class PauseMenuManager : MonoBehaviour
 
             // Reactivar el movimiento del jugador
             playerController.GetComponent<Rigidbody>().isKinematic = false;
+
+            // Reactivar el Timer
+            timerManager.StartTimer();
 
             // Ocultar el cursor cuando el Pause Menu no está activo
             Cursor.visible = false;
