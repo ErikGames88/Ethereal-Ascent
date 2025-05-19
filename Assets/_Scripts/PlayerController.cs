51bf9db8939927ce27211621a3237a03f434f2ab
ErikGames88 <erik.pan35@gmail.com>
2025-05-19 12:05:56 +0200

    Restart game!

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -2,28 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField,] private float speed;
-    [SerializeField] private float sprint;
-    [SerializeField] private Transform groundCheck;
-    [SerializeField] private float maxHeight = 4f;
-    [SerializeField] private float maxSprintTime = 2f;
-    [SerializeField] private float sprintRechargeRate = 10f;
-    [SerializeField] private float jumpForce = 5f;
-    [SerializeField] private float groundCheckRadius = 0.2f;
-    [SerializeField] private LayerMask groundLayer;
-    [SerializeField] private LayerMask terrainLayer;
-    [SerializeField] private Transform startPoint;
-
+    [Header("Movement")]
+    private float horizontal;
+    private float vertical;
+    private float currentSpeed;
+    [SerializeField] private float speed;
+    [SerializeField] private float sprintSpeed;
     private Rigidbody _rigidbody;
-    private float currentSprintTime = 0;
-    private bool canSprint = true;
-    private bool isSprintAllowed = true;
-    private bool isJumpAllowed = true;
-    private bool isGrounded;
-    private LayerMask combinedLayers; 
-
+    bool isGrounded;
 
 
     void Awake()
@@ -31,147 +20,52 @@ public class PlayerController : MonoBehaviour
         _rigidbody = GetComponent<Rigidbody>();
     }
 
-    void Start()
-    {
-        currentSprintTime = maxSprintTime;
-        combinedLayers = groundLayer | terrainLayer;
-
-        MoveToStartPoint();
-    }
-
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded && isJumpAllowed)
-        {
-            PlayerJump();
-        }
-    }
-
     void FixedUpdate()
     {
         PlayerMovement();
-
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundCheckRadius, combinedLayers);
-        if (transform.position.y > maxHeight)
-        {
-            Vector3 clampedPosition = transform.position;
-            clampedPosition.y = maxHeight;
-            transform.position = clampedPosition;
-        }
     }
 
-    private void PlayerMovement()
+    void PlayerMovement()
     {
-        if (_rigidbody.isKinematic)
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        bool isSprinting = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+        float yVelocity = _rigidbody.velocity.y;
+        Vector3 movement = (transform.right * horizontal + transform.forward * vertical).normalized;
+
+        if (!isSprinting)
         {
-            return;
-        }
-
-        float moveHorizontal = Input.GetAxis("Horizontal"); 
-        float moveVertical = Input.GetAxis("Vertical");     
-
-        Vector3 direction = transform.right * moveHorizontal + transform.forward * moveVertical;
-
-        float currentSpeed;
-
-        if (Input.GetKey(KeyCode.LeftShift) && canSprint && isSprintAllowed && moveVertical > 0 && moveHorizontal == 0)
-        {
-            currentSpeed = sprint;
-            currentSprintTime -= maxSprintTime / maxSprintTime * Time.fixedDeltaTime;
-
-            if (currentSprintTime <= 0)
-            {
-                canSprint = false;
-                currentSprintTime = 0;
-            }
+            currentSpeed = speed;
         }
         else
         {
-            currentSpeed = speed;
-
-            if (currentSprintTime < maxSprintTime)
-            {
-                currentSprintTime += maxSprintTime / sprintRechargeRate * Time.fixedDeltaTime;
-
-                if (currentSprintTime >= maxSprintTime)
-                {
-                    currentSprintTime = maxSprintTime;
-                    canSprint = true;
-                }
-            }
+            currentSpeed = sprintSpeed;
         }
 
-        Vector3 finalVelocity = direction * currentSpeed;
-        finalVelocity.y = _rigidbody.velocity.y;
-        _rigidbody.velocity = finalVelocity;
-    }
-
-    public float GetStamina()
-    {
-        return currentSprintTime;
-    }
-
-    public float GetMaxStamina()
-    {
-        return maxSprintTime;
-    }
-
-    private void PlayerJump()
-    {
-        _rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-    }
-
-    public bool IsGrounded()
-    {
-        return isGrounded;
-    }
-
-    public bool IsSprinting()
-    {
-        return Input.GetKey(KeyCode.LeftShift) && canSprint && isSprintAllowed;
-    }
-
-    public bool IsSprintAllowed()
-    {
-        return isSprintAllowed;
-    }
-
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.layer == LayerMask.NameToLayer("NoSprintZone"))
+        if (isGrounded)
         {
-            isSprintAllowed = false;
-            isJumpAllowed = false;
-
-            RainManager rainManager = GetComponentInChildren<RainManager>();
-            if (rainManager != null)
-            {
-                rainManager.SetRainActive(false);
-            }
+            _rigidbody.velocity = new Vector3(movement.x * currentSpeed, yVelocity, movement.z * currentSpeed);
         }
+
+        
+
     }
 
-    void OnTriggerExit(Collider other)
+    void OnCollisionEnter(Collision other)
     {
-        if (other.gameObject.layer == LayerMask.NameToLayer("NoSprintZone"))
+        if (other.gameObject.CompareTag("Ground"))
         {
-            isSprintAllowed = true;
-            isJumpAllowed = true;
-
-            RainManager rainManager = GetComponentInChildren<RainManager>();
-            if (rainManager != null)
-            {
-                rainManager.SetRainActive(true);
-            }
+            isGrounded = true;
         }
     }
 
-    public void MoveToStartPoint()
+    void OnCollisionExit(Collision other)
     {
-        if (startPoint != null)
+        if (other.gameObject.CompareTag("Ground"))
         {
-            transform.position = startPoint.position;
-            transform.rotation = startPoint.rotation;
+            isGrounded = false;
         }
     }
+
 }
